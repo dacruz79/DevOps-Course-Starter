b14993e945440c729052e396dda077df298fb191
Nouvel.Barreto <nbarreto@talk21.com>
2023-09-12 10:39:46 +0100

    Exercise 5 - Module 2: Tidy up code and add README

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,17 +1,13 @@
-from flask import Flask, render_template, session, request, redirect, url_for, json
+from flask import Flask, render_template, request, redirect, url_for
 
 from todo_app.flask_config import Config
-
-from todo_app.data.session_items import get_items, add_item
 
 from todo_app.data.trello_items import get_to_do_items, add_item_to_list, change_list_of_item, get_board_list
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
 @app.route('/')
-
 
 def index():
     get_board_list()
@@ -21,17 +17,15 @@ def index():
 @app.route('/add_new_item', methods=['post'])
 def add_new_item():
 
-    list_name = request.form['list_name']
-    item_title = request.form['item_title']
+    list_status = request.form['list_name']
+    item_name = request.form['item_title']
     item_description = request.form['item_desc']
 
-    if list_name == '':
-        list_name = 'To Do'
-    if item_title == '':
+    if item_name == '':
         error_text = 'No title provided!'
         return (error_text)
     
-    add_item_to_list(item_title, item_description,list_name)
+    add_item_to_list(item_name, item_description,list_status)
     return redirect(url_for('index'))
 
 @app.route('/complete', methods=['post'])
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -8,18 +8,17 @@ dotenv.load_dotenv()
 
 trello_key = os.getenv("API_KEY")
 trello_api_token = os.getenv("API_TOKEN")
+board_id = os.getenv("BOARD_ID")
 list_name = {}
 
 def get_board_list():
     """
-    Fetches id for the name from trello
+    Fetches details from trello for a board
 
     Returns:
-        id: The id of the items.
+        name: The name of the item.
+        id:   The id of the items.
     """
-
-    # DevOps BOW board, look to pass this in to make more dynamic
-    board_id = "64f9997df2894da1f3f1c056"
 
     reqUrl = f"https://api.trello.com/1/boards/{board_id}/lists"
 
@@ -41,14 +40,11 @@ def get_board_list():
 
 def get_to_do_items():
     """
-    Fetches all saved items from trello
+    Fetches all items from trello board.
 
     Returns:
-        list: The list of saved items.
+        list: The list of items.
     """
-
-    # DevOps BOW board, look to pass this in to make more dynamic
-    board_id = "64f9997df2894da1f3f1c056"
 
     reqUrl = f"https://api.trello.com/1/boards/{board_id}/lists"
 
@@ -56,40 +52,33 @@ def get_to_do_items():
         "key": trello_key,
         "token": trello_api_token,
         "cards": "open"
-        #"card_fields": "id,closed,name"
     }
 
     response = requests.get(reqUrl, params=query_params)
 
     response_json = response.json()
 
-    ##l = 1
     obj_list = []
     for trello_list in response_json:
         for card in trello_list['cards']:
-            ##globals()[f'item"_{l}'] = Item.from_trello_card(card, trello_list)
-            ##i_1 = Item.from_trello_card(card, trello_list)
-            ##print(i_1.name + ' ' + i_1.id + ' ' + i_1.status)
-            ##l += 1
-            #obj_list.append(globals()[f'item_{l}'])
             obj_list.append(Item.from_trello_card(card, trello_list))
 
     return obj_list 
 
 
-def add_item_to_list(title, description, add_to_list_name = 'To Do'):
+def add_item_to_list(title, description, list_status = 'To Do'):
     """
     Adds a new item with the specified title to trello board.
 
     Args:
-        title: The title of the item.
-        description: The description of the item.
-        add_to_list_name: List to add item to.
+        title:        The title of the item.
+        description:  The description of the item.
+        list_status:  List to add item to, defaulted to To Do
     Returns:
         item: The saved item.
     """
 
-    list_id = list_name[add_to_list_name]
+    list_id = list_name[list_status]
 
     reqUrl = f"https://api.trello.com/1/cards"
     
@@ -107,24 +96,24 @@ def add_item_to_list(title, description, add_to_list_name = 'To Do'):
     response_json = response.json()
     return response_json
 
-def change_list_of_item(card_id, changed_list_name):
+def change_list_of_item(card_id, list_status):
     """
     Change the list the item should fall under on the trello board.
 
     Args:
-        card_id: The id of the item.
-        changed_list_name: The name of the list to go to.
+        card_id:     The id of the item.
+        list_status: The name of the list to go to.
     Returns:
         item: The saved item.
     """
 
-    new_list_id = list_name[changed_list_name]
+    list_id = list_name[list_status]
     reqUrl = f"https://api.trello.com/1/cards/{card_id}"
 
     query_params = {
         "key": trello_key,
         "token": trello_api_token,
-        "idList": new_list_id
+        "idList": list_id
     }
 
     response = requests.put(reqUrl, params=query_params)
